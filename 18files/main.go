0f6c02b539eb033163b1f9480d70179a7203b682
Abir Khan <ak99.abirkhan@gmail.com>
2026-03-26 14:55:41 +0600

    lesson Read & Write in Go :)

---
+++ 18files/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("Files in Go!")
+
+	//write in file
+	writeFile()
+
+	readFile("./lcofile.text")
+
+}
+
+func writeFile() {
+
+	content := "This is the content for file write in Go! :)"
+
+	file, err := os.Create("lcofile.text")
+
+	checkNilErr(err)
+
+	len, err := io.WriteString(file, content)
+
+	checkNilErr(err)
+
+	fmt.Println("Length of the file: ", len)
+
+	defer file.Close()
+}
+
+
+func readFile(file string) {
+
+	conent, err := os.ReadFile(file)
+
+	checkNilErr(err)
+
+	fmt.Println("Content is: ", string(conent))
+}
+
+
+
+
+func checkNilErr(err error) {
+
+	if err != nil {
+		panic(err)
+	}
+}

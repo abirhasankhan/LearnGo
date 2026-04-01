0474c4d3ec001118732ac741dc3c6c0436b4dd6f
Abir Khan <ak99.abirkhan@gmail.com>
2026-04-01 14:38:23 +0600

    post request

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -7,18 +7,20 @@ import (
 	"strings"
 )
 
+var baseurl string = "http://localhost:8000/"
+
 func main() {
 
 	fmt.Println("Web Request Server !")
 
-	PerformGetRequest("http://localhost:8000/get")
+	// PerformGetRequest(baseurl + "get")
+
+	PerformPostJSONRequest(baseurl + "post")
 }
 
 func PerformGetRequest(url string) {
 
-	myurl := url
-
-	res, err := http.Get(myurl)
+	res, err := http.Get(url)
 
 	if err != nil {
 
@@ -32,7 +34,7 @@ func PerformGetRequest(url string) {
 
 	content, _ := io.ReadAll(res.Body)
 
-	// fmt.Println(string(content)) 
+	// fmt.Println(string(content))
 
 	var resString strings.Builder
 
@@ -43,5 +45,32 @@ func PerformGetRequest(url string) {
 	}
 
 	fmt.Println(resString.String())
+
+}
+
+func PerformPostJSONRequest(url string) {
+
+	// fake JSON payload
+
+	reqestBody := strings.NewReader(`
+		{
+			"course": "Let's go with Golang",
+			"price": 240,
+			"platform":"n/a"
+
+		}
+	`)
+
+	res, err := http.Post(url, "application/json", reqestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	content, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(content))
 
 }

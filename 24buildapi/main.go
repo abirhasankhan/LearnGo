f209b48e55aba46e298292831304791bf05bf80d
Abir Khan <ak99.abirkhan@gmail.com>
2026-04-10 02:51:47 +0600

    Sending a API json response for all courses in golang

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 
 type Course struct {
 	CourseId string `json:"courseid"`
 	CourseName string `json:"course"`
 	Price int `json:"price"`
+	Author *Author `json:"author"`
 }
 
 type Author struct {
@@ -15,6 +20,8 @@ type Author struct {
 }
 
 
+var courses []Course 
+
 func (c *Course) IsEmpty() bool {
 
 	return c.CourseId == "" && c.CourseName == ""
@@ -22,4 +29,18 @@ func (c *Course) IsEmpty() bool {
 
 func main() {
 	fmt.Println("Build API")
+}
+
+
+//controller 
+
+func serverHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Welcome to server home by Go!</h1>"))
+}
+
+
+func getAllCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get All the course")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
